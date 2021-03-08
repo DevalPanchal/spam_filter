b957ad5cbc44e76842515c8828275313030ebac5
DevalPanchal2401 <devalp2401@gmail.com>
2021-03-08 18:27:38 -0500

    populated table with files and probability

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.DirectoryChooser;
 
 import java.io.File;
@@ -57,9 +58,37 @@ public class Controller {
             String path = mainDirectory.getAbsolutePath();
             testDataPath.setText(path);
 
-            wordCounter.parseTestingFolder(mainDirectory);
+            parseTestingFolder(mainDirectory);
 
 
+
+            fileColumn.setCellValueFactory(new PropertyValueFactory<>("filename"));
+            actualClassColumn.setCellValueFactory(new PropertyValueFactory<>("actualClass"));
+            spamProbabilityColumn.setCellValueFactory(new PropertyValueFactory<>("spamProbability"));
+        } else {
+            System.out.println("Directory not valid!");
+        }
+    }
+
+    public void parseTestingFolder(File file) throws IOException {
+        String fileName = file.getPath();
+        if (file.isDirectory()) {
+            File[] content = file.listFiles();
+            for (File current: content) {
+                parseTestingFolder(current);
+            }
+        } else {
+            double probabilitySpam = 0.0;
+            try {
+                probabilitySpam = wordCounter.probabilitySpamFile(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (fileName.contains("ham")) {
+                tableView.getItems().add(new TestFile(file.getName(), probabilitySpam, "ham"));
+            } else if (fileName.contains("spam")) {
+                tableView.getItems().add(new TestFile(file.getName(), probabilitySpam, "spam"));
+            }
         }
     }
 }
