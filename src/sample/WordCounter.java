40d42dfbd657fe6ec8b4040810534ad6aa57dedb
DevalPanchal2401 <devalp2401@gmail.com>
2021-03-08 10:34:40 -0500

    Fixing bugs

--- src/sample/WordCounter.java
+++ src/sample/WordCounter.java
@@ -13,12 +13,16 @@ public class WordCounter {
     private Map<String, Integer> trainHamFreq;
     private Map<String, Integer> trainSpamFreq;
     private Map<String, Double> hamProbability;
+    private Map<String, Double> spamProbability;
+    private Map<String, Double> probabilityOfSpamFile;
 
     public WordCounter(){
         wordCounts = new TreeMap<>();
         trainHamFreq = new TreeMap<>();
         trainSpamFreq = new TreeMap<>();
         hamProbability = new TreeMap<>();
+        spamProbability = new TreeMap<>();
+        probabilityOfSpamFile = new TreeMap<>();
     }
 
     public Map<String, Integer> getWordCounts() {
@@ -51,10 +55,16 @@ public class WordCounter {
                 if (isValidWord(token)) {
                     if (filePathName.contains("ham")) {
                         hamFrequency(token);
+                        probabilityWordAppearsInHam(token, 2500);
+                    } else if (filePathName.contains("ham2")) {
+                        hamFrequency(token);
+                        probabilityWordAppearsInHam(token, 250);
                     } else if (filePathName.contains("spam")) {
                         spamFrequency(token);
+                        probabilityWordAppearsInSpam(token);
                     }
-
+//                    System.out.println("spam: " + spamProbability.get(token));
+//                    System.out.println("ham: " + hamProbability.get(token));
                 }
             }
         }
@@ -93,21 +103,34 @@ public class WordCounter {
         }
     }
 
+    public void probabilityWordAppearsInHam(String word, double numFiles) {
+        double probability = (double) trainHamFreq.get(word) / (double) numFiles;
+        hamProbability.put(word, probability);
+    }
 
+    public void probabilityWordAppearsInSpam(String word) {
+        double probability = (double) trainSpamFreq.get(word) / (double) 500;
+        spamProbability.put(word, probability);
+    }
+
+    public void probabilityFileIsSpam(String word) {
+        double probability = spamProbability.get(word) / spamProbability.get(word) + hamProbability.get(word);
+        probabilityOfSpamFile.put(word, probability);
+    }
 
     public void outputWordCount(int minCount, File output) throws IOException{
         System.out.println("Saving word counts to file:" + output.getAbsolutePath());
         System.out.println("Total words:" + wordCounts.keySet().size());
 
-        if (!output.exists()){
+        if (!output.exists()) {
             output.createNewFile();
-            if (output.canWrite()){
+            if (output.canWrite()) {
                 PrintWriter fileOutput = new PrintWriter(output);
 
                 Set<String> keys = wordCounts.keySet();
                 Iterator<String> keyIterator = keys.iterator();
 
-                while(keyIterator.hasNext()){
+                while(keyIterator.hasNext()) {
                     String key = keyIterator.next();
                     int count = wordCounts.get(key);
                     // testing minimum number of occurances
@@ -117,11 +140,10 @@ public class WordCounter {
                 }
                 fileOutput.close();
             }
-        }else{
+        } else {
             System.out.println("Error: the output file already exists: " + output.getAbsolutePath());
         }
     }
-
 
     //main method
     public static void main(String[] args) {
@@ -131,24 +153,28 @@ public class WordCounter {
         }
 
         File dataDir = new File(args[0]);
-        //File dataDir2 = new File(args[1]);
+        File dataDir2 = new File(args[1]);
         //File outFile = new File(args[1]);
 
         WordCounter wordCounter = new WordCounter();
         System.out.println("Hello");
         try{
             wordCounter.parseFile(dataDir);
-
+            wordCounter.parseFile(dataDir2);
             //System.out.println("Testing if this works: " + wordCounter.hamProbability);
+            //System.out.println("HOPING: " + wordCounter.trainHamFreq);
+            //System.out.println("REALLY AM HOPING: " + wordCounter.trainSpamFreq);
 
-            System.out.println("HOPING: " + wordCounter.trainHamFreq);
-            System.out.println("REALLY AM HOPING: " + wordCounter.trainSpamFreq);
+//            System.out.println("hamProbability: " + wordCounter.hamProbability);
+//            System.out.println("spamProbability: " + wordCounter.spamProbability);
+
+            System.out.println("probabilityOfSpamFile: " + wordCounter.probabilityOfSpamFile);
             //wordCounter.parseFile(dataDir2);
             //wordCounter.outputWordCount(2, outFile);
-        }catch(FileNotFoundException e){
+        }catch(FileNotFoundException e) {
             System.err.println("Invalid input dir: " + dataDir.getAbsolutePath());
             e.printStackTrace();
-        }catch(IOException e){
+        }catch(IOException e) {
             e.printStackTrace();
         }
     }
